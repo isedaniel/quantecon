54978e046410be67930de4388b155a1d2355655d
Daniel <ise.daniel@gmail.com>
2023-12-13 14:07:49 -0300

    reorder

---
+++ exercises/0-python-programming/4/1.py
@@ -0,0 +1,13 @@
+# %matplotlib inline
+# import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['figure.figsize'] = (10, 6)
+
+
+def f(x, a=1, b=1):
+    return a*x + b
+
+
+print(f(1))
+print(f(2))
