5521aeb0459c3f5f8fc4fd721e7debecf5719e30
Daniel <ise.daniel@gmail.com>
2023-12-15 13:44:07 -0300

    first exercise

--- exercises/0-python-programming/4/1.py
+++ exercises/0-python-programming/4/1.py
@@ -1,13 +1,21 @@
 # %matplotlib inline
 # import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-plt.rcParams['figure.figsize'] = (10, 6)
-
-
-def f(x, y, a=1, b=1):
-    return a*x + b
+#plt.rcParams['figure.figsize'] = (10, 6)
 
 
-print(f(y=1, x=3, a=2))
-print(f(2, 2))
+def factorial(n, f=lambda x: x):
+    if n <= 2:
+        return n
+
+    return f(n) * factorial(f(n) - 1, f)
+
+
+n = 5
+for i in range(n+1):
+    print(f"{i}: {factorial(i)}")
+
+
+for i in range(n+1):
+    print(f"{i}: {factorial(i, lambda x: x + 1)}")
