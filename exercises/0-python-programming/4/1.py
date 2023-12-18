2302daad461ebe9f6df209908efdebf01f1f4916
Daniel <ise.daniel@gmail.com>
2023-12-18 09:56:43 -0300

    first working ver

--- exercises/0-python-programming/4/1.py
+++ exercises/0-python-programming/4/1.py
@@ -1,21 +1,30 @@
 # %matplotlib inline
 # import numpy as np
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-#plt.rcParams['figure.figsize'] = (10, 6)
+# plt.rcParams['figure.figsize'] = (10, 6)
 
 
 def factorial(n, f=lambda x: x):
+    n = f(n)
+
     if n <= 2:
         return n
 
-    return f(n) * factorial(f(n) - 1, f)
+    return n * factorial(n - 1)
+
+
+def f(x):
+    if x % 2:  # if x is odd
+        return x ** 2
+    return x ** 2 + 1
 
 
 n = 5
 for i in range(n+1):
     print(f"{i}: {factorial(i)}")
 
+print()
 
 for i in range(n+1):
-    print(f"{i}: {factorial(i, lambda x: x + 1)}")
+    print(f"{f(i)}: {factorial(i, f)}")
