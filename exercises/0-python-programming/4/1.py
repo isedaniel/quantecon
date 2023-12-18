37a65f7cd355119ce8cfba555f20d4160f89c6c0
Daniel <ise.daniel@gmail.com>
2023-12-18 09:59:58 -0300

    final cleaning

--- exercises/0-python-programming/4/1.py
+++ exercises/0-python-programming/4/1.py
@@ -1,9 +1,3 @@
-# %matplotlib inline
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# plt.rcParams['figure.figsize'] = (10, 6)
-
 
 def factorial(n, f=lambda x: x):
     n = f(n)
@@ -20,11 +14,12 @@ def f(x):
     return x ** 2 + 1
 
 
+# some test with n numbers
 n = 5
 for i in range(n+1):
-    print(f"{i}: {factorial(i)}")
+    print(f"{ i }: { factorial(i) }")
 
-print()
+print()  # new line
 
 for i in range(n+1):
-    print(f"{f(i)}: {factorial(i, f)}")
+    print(f"{ f(i) }: { factorial(i, f) }")
