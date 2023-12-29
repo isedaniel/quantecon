f45082026f594821a94d36a0759e84021094aede
Daniel <ise.daniel@gmail.com>
2023-12-29 10:36:42 -0300

    market surplus with and without taxes

--- exercises/0-python-programming/7/market_test.py
+++ exercises/0-python-programming/7/market_test.py
@@ -1,22 +1,31 @@
 import market
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+
+def deadw(m):
+    m_no_tax = market.Market(m.ad, m.bd, m.az, m.bz, 0)
+    surp1 = m_no_tax.consumer_surp() + m_no_tax.producer_surp()
+    surp2 = m.consumer_surp() + m.producer_surp() + m.taxrev()
+    return surp1 - surp2
+
 
 params = 15, .5, -2, .5, 3
 m = market.Market(*params)
+print(deadw(m))
 
-q_max = m.quantity() * 2
-q_grid = np.linspace(0.0, q_max, 100)
-pd = m.inverse_demand(q_grid)
-ps = m.inverse_supply(q_grid)
-psno = m.inverse_supply_no_tax(q_grid)
+# q_max = m.quantity() * 2
+# q_grid = np.linspace(0.0, q_max, 100)
+# pd = m.inverse_demand(q_grid)
+# ps = m.inverse_supply(q_grid)
+# psno = m.inverse_supply_no_tax(q_grid)
 
-fig, ax = plt.subplots()
-ax.plot(q_grid, pd, lw=2, alpha=0.6, label='demand')
-ax.plot(q_grid, ps, lw=2, alpha=0.6, label='supply')
-ax.plot(q_grid, psno, '--k', lw=2, alpha=0.6, label='supply without tax')
-ax.set_xlabel('quantity', fontsize=14)
-ax.set_xlim(0, q_max)
-ax.set_ylabel('price', fontsize=14)
-ax.legend(loc='lower right', frameon=False, fontsize=14)
-plt.show()
+# fig, ax = plt.subplots()
+# ax.plot(q_grid, pd, lw=2, alpha=0.6, label='demand')
+# ax.plot(q_grid, ps, lw=2, alpha=0.6, label='supply')
+# ax.plot(q_grid, psno, '--k', lw=2, alpha=0.6, label='supply without tax')
+# ax.set_xlabel('quantity', fontsize=14)
+# ax.set_xlim(0, q_max)
+# ax.set_ylabel('price', fontsize=14)
+# ax.legend(loc='lower right', frameon=False, fontsize=14)
+# plt.show()
